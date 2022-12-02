b6deb6ecef3455e191b3e669417ec2282a8ba620
J. Q <55899496+jawadqur@users.noreply.github.com>
2022-12-02 10:08:16 -0600

    Add WTS OIDC job, Read DB connection from env vars (#66)
    * Add wts OIDC job, all db connection is read from env
    
    * Add alb capabilites to ingress
    
    * Fix fence-config

--- helm/indexd/indexd-settings/local_settings.py
+++ helm/indexd/indexd-settings/local_settings.py
@@ -2,7 +2,6 @@ from indexd.index.drivers.alchemy import SQLAlchemyIndexDriver
 from indexd.alias.drivers.alchemy import SQLAlchemyAliasDriver
 from indexd.auth.drivers.alchemy import SQLAlchemyAuthDriver
 
-import config_helper
 
 from os import environ
 import json
@@ -10,20 +9,18 @@ import json
 APP_NAME = "indexd"
 
 
-
 usr = environ.get("PGUSER", "indexd")
 db = environ.get("PGDB", "indexd")
 psw = environ.get("PGPASSWORD")
 pghost = environ.get("PGHOST")
 pgport = environ.get("PGPORT", 5432)
-# TODO: FIX THIS TO rEAD FROM ENV VARS TOO
-# "index_config": {
-#     "DEFAULT_PREFIX": "BRH",
-#     "PREPEND_PREFIX": true
-#   },
-index_config = environ.get("INDEX_CONFIG", {"DEFAULT_PREFIX": "BRH",
-    "PREPEND_PREFIX": true
-  })
+
+# TODO: FIX THIS TO READ FROM ENV VARS
+index_config = {
+    "DEFAULT_PREFIX": environ.get("DEFAULT_PREFIX", "testprefix"),
+    "PREPEND_PREFIX": environ.get("PREPEND_PREFIX", True),
+}
+
 CONFIG = {}
 
 CONFIG["JSONIFY_PRETTYPRINT_REGULAR"] = False
