74809841af040de065cea3e7fe4b2a395dd77af6
Jawad Qureshi <qureshi@uchicago.edu>
2025-06-27 11:49:18 -0500

    Add single table configuration

--- helm/indexd/indexd-settings/local_settings.py
+++ helm/indexd/indexd-settings/local_settings.py
@@ -1,12 +1,14 @@
 from indexd.index.drivers.alchemy import SQLAlchemyIndexDriver
 from indexd.alias.drivers.alchemy import SQLAlchemyAliasDriver
 from indexd.auth.drivers.alchemy import SQLAlchemyAuthDriver
+from .index.drivers.single_table_alchemy import SingleTableSQLAlchemyIndexDriver
 
 
 from os import environ
 import json
 
 APP_NAME = "indexd"
+
 
 
 usr = environ.get("PGUSER", "indexd")
@@ -31,14 +33,39 @@ if dist:
 
 arborist = environ.get("ARBORIST", "false").lower() == "true"
 
-CONFIG["INDEX"] = {
-    "driver": SQLAlchemyIndexDriver(
-        "postgresql+psycopg2://{usr}:{psw}@{pghost}:{pgport}/{db}".format(
-            usr=usr, psw=psw, pghost=pghost, pgport=pgport, db=db
-        ),
-        index_config=index_config,
-    )
-}
+USE_SINGLE_TABLE = environ.get("USE_SINGLE_TABLE", False)
+
+# - DEFAULT_PREFIX: prefix to be prepended.
+# - PREPEND_PREFIX: the prefix is preprended to the generated GUID when a
+#   new record is created WITHOUT a provided GUID.
+# - ADD_PREFIX_ALIAS: aliases are created for new records - "<PREFIX><GUID>".
+# Do NOT set both ADD_PREFIX_ALIAS and PREPEND_PREFIX to True, or aliases
+# will be created as "<PREFIX><PREFIX><GUID>".
+if USE_SINGLE_TABLE is True:
+    CONFIG["INDEX"] = {
+        "driver": SingleTableSQLAlchemyIndexDriver(
+            "postgresql://postgres:postgres@localhost:5432/indexd_tests",  # pragma: allowlist secret
+            echo=True,
+            index_config={
+                "DEFAULT_PREFIX": "testprefix:",
+                "PREPEND_PREFIX": True,
+                "ADD_PREFIX_ALIAS": False,
+            },
+        )
+    }
+else:
+    CONFIG["INDEX"] = {
+        "driver": SQLAlchemyIndexDriver(
+            "postgresql://postgres:postgres@localhost:5432/indexd_tests",  # pragma: allowlist secret
+            echo=True,
+            index_config={
+                "DEFAULT_PREFIX": "testprefix:",
+                "PREPEND_PREFIX": True,
+                "ADD_PREFIX_ALIAS": False,
+            },
+        )
+    }
+
 
 CONFIG["ALIAS"] = {
     "driver": SQLAlchemyAliasDriver(
