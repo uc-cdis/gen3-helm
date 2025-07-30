e626c29a62e03b1d87abe65cf1bb43f1d459be7e
paulmurdoch19 <paulmurdoch19@gmail.com>
2025-07-30 15:35:12 -0700

    Refactor secret handling and update Helm chart configs
    Removed in-chart secret generation and patching for gearbox and sheepdog, shifting secret management outside the Helm charts. Updated environment variable handling and volume mounts to use externally managed secrets. Upgraded common chart dependency, improved sheepdog settings.py for better config sourcing, and made image/tag/pullPolicy values more explicit for several services. Also removed unused nginx server block and improved .gitignore formatting.

--- helm/sheepdog/sheepdog-secret/settings.py
+++ helm/sheepdog/sheepdog-secret/settings.py
@@ -1,6 +1,7 @@
 from sheepdog.api import app, app_init
 from os import environ
-import confighelper
+import os
+import bin.confighelper as confighelper
 
 APP_NAME = "sheepdog"
 
@@ -12,45 +13,58 @@ def load_json(file_name):
 conf_data = load_json("creds.json")
 config = app.config
 
-config["AUTH"] = "https://auth.service.consul:5000/v3/"
-config["AUTH_ADMIN_CREDS"] = None
-config["INTERNAL_AUTH"] = None
-
 # ARBORIST deprecated, replaced by ARBORIST_URL
 # ARBORIST_URL is initialized in app_init() directly
 config["ARBORIST"] = "http://arborist-service/"
 
-# Signpost: deprecated, replaced by index client.
-config["SIGNPOST"] = {
-    "host": environ.get("SIGNPOST_HOST") or "http://indexd-service",
-    "version": "v0",
-    "auth": ("gdcapi", conf_data.get("indexd_password", "{{indexd_password}}")),
-}
 config["INDEX_CLIENT"] = {
-    "host": environ.get("INDEX_CLIENT_HOST") or "http://indexd-service",
+    "host": os.environ.get("INDEX_CLIENT_HOST") or "http://indexd-service",
     "version": "v0",
-    "auth": ("gdcapi", conf_data.get("indexd_password", "{{indexd_password}}")),
+    # The user should be "sheepdog", but for legacy reasons, we use "gdcapi" instead
+    "auth": (
+        (
+            environ.get("INDEXD_USER", "gdcapi"),
+            environ.get("INDEXD_PASS")
+            or conf_data.get("indexd_password", "{{indexd_password}}"),
+        )
+    ),
 }
-config["FAKE_AUTH"] = False
+
 config["PSQLGRAPH"] = {
-    "host": environ.get("PGHOST"),
-    "user": environ.get("PGUSER"),
-    "password": environ.get("PGPASSWORD"),
-    "database": environ.get("PGDB"),
+    "host": conf_data.get("db_host", os.environ.get("PGHOST", "localhost")),
+    "user": conf_data.get("db_username", os.environ.get("PGUSER", "sheepdog")),
+    "password": conf_data.get("db_password", os.environ.get("PGPASSWORD", "sheepdog")),
+    "database": conf_data.get("db_database", os.environ.get("PGDB", "sheepdog")),
 }
 
 config["FLASK_SECRET_KEY"] = conf_data.get("gdcapi_secret_key", "{{gdcapi_secret_key}}")
-config['PSQL_USER_DB_CONNECTION'] = 'postgresql://%s:%s@%s:5432/%s' % (environ.get("FENCE_DB_USER"), environ.get("FENCE_DB_PASS"), environ.get("FENCE_DB_HOST"), environ.get("FENCE_DB_DBNAME"))
+fence_username = conf_data.get(
+    "fence_username", os.environ.get("FENCE_DB_USER", "fence")
+)
+fence_password = conf_data.get(
+    "fence_password", os.environ.get("FENCE_DB_PASS", "fence")
+)
+fence_host = conf_data.get("fence_host", os.environ.get("FENCE_DB_HOST", "localhost"))
+fence_database = conf_data.get(
+    "fence_database", os.environ.get("FENCE_DB_DATABASE", "fence")
+)
+config["PSQL_USER_DB_CONNECTION"] = "postgresql://%s:%s@%s:5432/%s" % (
+    fence_username,
+    fence_password,
+    fence_host,
+    fence_database,
+)
 
-config["BASE_URL"] = "https://%s/user" % conf_data["hostname"] # for use by authutils remove when authutils gets updated 
-config["USER_API"] = "http://fence-service/"  # for use by authutils og: "https://%s/user" % conf_data["hostname"]
+config["USER_API"] = "https://%s/user" % conf_data.get(
+    "hostname", os.environ.get("CONF_HOSTNAME", "localhost")
+)  # for use by authutils
 # use the USER_API URL instead of the public issuer URL to accquire JWT keys
 config["FORCE_ISSUER"] = True
-config["DICTIONARY_URL"] = environ.get(
+config["DICTIONARY_URL"] = os.environ.get(
     "DICTIONARY_URL",
     "https://s3.amazonaws.com/dictionary-artifacts/datadictionary/develop/schema.json",
 )
 
 app_init(app)
 application = app
-application.debug = environ.get("GEN3_DEBUG") == "True"
+application.debug = os.environ.get("GEN3_DEBUG") == "True"
